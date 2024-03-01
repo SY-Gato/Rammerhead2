2bbbc72499925dbe53d94577c1d7613917ac64f0
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-02-29 23:37:37 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,32 +1,14 @@
 let ip = "127.0.0.1";
-let port = 4040;
+let portNum = 4040;
 
-var url = require('url');
-var WebSocket = require('ws');
-var HttpsProxyAgent = require('https-proxy-agent');
+import { WebSocketServer } from 'ws';
 
-// HTTP/HTTPS proxy to connect to
-var proxy = "http://127.0.0.1:4040"; //'http://168.63.76.32:3128';
-console.log('using proxy server %j', proxy);
+const wss = new WebSocketServer({ port: portNum });
 
-// WebSocket endpoint for the proxy to connect to
-var endpoint = 'ws://echo.websocket.org';
-console.log('attempting to connect to WebSocket %j', endpoint);
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
 
-// create an instance of the `HttpsProxyAgent` class with the proxy server information
-var options = url.parse(proxy);
-
-var agent = new HttpsProxyAgent(options);
-
-// finally, initiate the WebSocket connection
-var socket = new WebSocket(endpoint, { agent: agent });
-
-socket.on('open', function () {
-  console.log('"open" event!');
-  socket.send('hello world');
-});
-
-socket.on('message', function (data, flags) {
-  console.log('"message" event! %j %j', data, flags);
-  socket.close();
+  ws.send('something');
 });
