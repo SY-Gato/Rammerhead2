2b0d34302bbefa962e27b4098b714384e478b10e
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-03-01 08:10:20 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const { WebSocketServer } = require('ws');
 
 const wss = new WebSocketServer({ port: portNum });
 wss.onopen = (event) => {
-  console.log("Running on port "+toSring(portNum);
+  console.log("Running on port "+toSring(portNum));
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
     console.log('received: %s', data);
