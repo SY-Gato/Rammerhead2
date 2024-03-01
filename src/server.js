0074327ba687ace3fe8a22a91ca379c523c41116
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-03-01 10:59:38 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,15 +3,18 @@ let portNum = 4040;
 
 const WebSocket = require('ws');
 console.log("SERVER.JS started");
-const wss = new WebSocketServer({ port: portNum });
+const wss = new WebSocket.Server({ port: portNum });
 //wss.on("open", function() {
 console.log(String(wss != null));
   console.log("Running on port "+String(portNum));
 wss.on('connection', function connection(ws) {
+  console.log("Client Connected!");
   ws.on('message', function message(data) {
     console.log('received: %s', data);
   });
 
-  ws.send('something');
+  ws.on("close", () => {
+    console.log("Client Disconnected.");
+  });
 });
 //});
